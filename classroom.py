28bde0c364a3cb57e50fc2a3659ac3cd5ad9f854
TAnderson-Guitar <toby.anderson9@education.nsw.gov.au>
2025-06-13 00:55:09 +0000

    Created Journal part 2 file and made all the rooms i need

--- classroom.py
+++ classroom.py
@@ -1,5 +1,5 @@
 """Cave Class for setup CAPA Showcasee"""
-class classroom:
+class Classroom:
     """Defines attributes and methods for classroom objects"""
     def __init__(self, classroom_name):
         """Sets the class attributes"""
@@ -9,5 +9,50 @@ class classroom:
         self.character = None
 
     def set_name(self, classroom_name):
-        """Sets the cave name"""
+        """Sets the classroom name"""
         self.name = classroom_name
+
+    def get_name(self):
+        """Gets the classroom name"""
+        return self.name
+
+    def set_description(self, classroom_description):
+        """Sets the classrooms description"""
+        self.description = classroom_description
+
+    def get_description(self):
+        """Gets the classrooms description"""
+        return self.description
+
+    def describe(self):
+        """Prints the classrooms description"""
+        print(self.description)
+
+    def link_classrooms(self, classroom_to_link, direction):
+        """Populates dictionary of linked classrooms"""
+        self.linked_classroom[direction] = classroom_to_link
+        #print(self.name + "linked caves:" + repr(self.linked_caves))
+
+    def get_details(self):
+        """Gets the classroom details"""
+        print(self.name)
+        print("----------")
+        print(self.description)
+        for direction, classroom in self.linked_classroom.items():
+            print("The " + classroom.get_name() + " is " + direction)
+
+    def move(self, direction):
+        """Allows the player to move throughout the classrooms"""
+        if direction in self.linked_classroom:
+            return self.linked_classroom[direction]
+        else:
+            print("You cannot go that way.")
+            return self
+
+    def set_character(self, new_character):
+        """Sets the character objects in the cave objects"""
+        self.character = new_character
+
+    def get_character(self):
+        """Returns thename of character object in this cave"""
+        return self.character
